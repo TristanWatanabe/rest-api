67914ac515c4ee0e8299c6c652118c9470da7258
Tristan Watanabe <tristan.watanabe@gmail.com>
2019-11-14 12:59:48 -0500

    fix: can now seed heroku database

--- server/db/db.js
+++ server/db/db.js
@@ -1,26 +1,11 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
 
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
-//   {
-//     logging: false
-//   }
-// );
-let db;
-
-if (process.env.DATABASE_URL) {
-  // the application is executed on Heroku ... use the postgres database
-  db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
+  {
     logging: false
-  });
-} else {
-  // the application is executed on the local machine
-  db = new Sequelize(`postgres://localhost:5432/${pkg.name}`, {
-    logging: false
-  });
-}
+  }
+);
 
 module.exports = db;
